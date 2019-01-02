f28988ed717c1c38ec9b8e2610758f335aedccd4
MayheMatan <MayheMatan@10.0.0.163>
2019-01-02 19:32:05 +0200

    fixes added

--- src/main/java/com/hit/server/HandleRequest.java
+++ src/main/java/com/hit/server/HandleRequest.java
@@ -4,152 +4,74 @@ package com.hit.server;
  * command to CacheUnitController to invoke the method base on the string.
  * */
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.hit.dm.DataModel;
 import com.hit.services.CacheUnitController;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.lang.reflect.Type;
 import java.net.Socket;
-import java.util.Map;
 import java.util.Scanner;
 
 public class HandleRequest<T> extends java.lang.Object implements Runnable
 {
-
-   private Socket socket;
-   private CacheUnitController<T> unitController;
-   private Request<DataModel<T>[]> socketRequest;
-   private ObjectInputStream input;
-   private ObjectOutputStream output;
-   private Gson gson;
-   private Type ref;
+	private static final String UPDATE = "update";
+	private static final String DELETE = "delete";
+	private static final String GET = "get";
+	private static final String ACTION = "action";
 
     //initialize request socket(from server class) and create a gson instance
-	public HandleRequest(java.net.Socket s, CacheUnitController<T> controller)
-    {
-        this.socket = s;
-        this.unitController = controller;
-        try
-        {
-        	Scanner reader = new Scanner(new InputStreamReader(socket.getInputStream()));
-        	PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-        } catch (IOException e)
-        {
-            e.printStackTrace ();
-        }
-        gson = new GsonBuilder().create();
-    }
-    @SuppressWarnings({ "rawtypes" })
+	private CacheUnitController<T> cacheUnitController;
+	private Socket socket;
+
+	public HandleRequest(Socket s, CacheUnitController<T> controller) {
+		this.cacheUnitController = controller;
+		this.socket = s;
+	}
+
 	@Override
-    public void run() {
-    	
-    	String inputString;
-        String command;
-        DataModel<T>[] body;
-        
-        try
-        {
-            ref = new TypeToken<Request<DataModel<T>[]>> (){}.getType();
-            inputString = (String)input.readObject();
-            socketRequest = new Gson().fromJson(inputString, ref);
+	public void run() {
 
-        } catch (IOException e)
-        {
-            e.printStackTrace ();
-        } catch (ClassNotFoundException e)
-        {
-            e.printStackTrace ();
-        }
-        //read headers from json and get the action
-        Map headers = socketRequest.getHeaders ();
-        command = (String)headers.get("action");
-        body = socketRequest.getBody();
+		Request<DataModel<T>[]> request = null;
 
-        //checks which action to pass to controller
-        if(command.equals("GET"))
-        {
-            DataModel[] dataModels = null;
-			try {
-				dataModels = unitController.get(body);
-			} catch (ClassNotFoundException | IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
+		try {
+			Scanner reader = new Scanner(new InputStreamReader(socket.getInputStream()));
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			String req = (String)reader.nextLine();
+		    Type ref = new TypeToken<Request<DataModel<T>[]>>() {}.getType();
+			request = new Gson().fromJson(req, ref);
 
-            for(DataModel dataModel: dataModels)
-            {
-                try
-                {
-                    String outputGson = gson.toJson(dataModel);
-                    output.writeObject(outputGson);
-                } catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-
-
-        }else if(command.equals("DELETE"))
-        {
-
-            boolean delete = false;
-			try {
-				try {
-					delete = unitController.delete(body);
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			String action = request.getHeaders().get(ACTION);
+			switch (action.toLowerCase()) {
+			case UPDATE:
+				boolean updateResult = this.cacheUnitController.update(request.getBody());
+				writer.println("Update Result=" + updateResult);
+				break;
+			case DELETE:
+				boolean deleteResult = this.cacheUnitController.delete(request.getBody());
+				writer.println("Delete Result=" + deleteResult);
+				break;
+			case GET:
+				DataModel<T>[] dms = this.cacheUnitController.get(request.getBody());
+				if (dms != null) {
+					writer.println(dms.toString());
+				}else {
+					writer.println( "Don't exist in the memory");
 				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+				break;
 			}
+			writer.flush();
+			reader.close();
+			writer.close();
+		} catch (
 
-            try
-            {
-                output.writeObject(delete);
-            } catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+		IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
-        }else if(command.equals("UPDATE"))
-        {
+	}
 
-            boolean update = false;
-			try {
-				update = unitController.update(body);
-			} catch (ClassNotFoundException | IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-
-            try
-            {
-                output.writeObject(update);
-            } catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-
-        }else
-        {
-            try
-            {
-                output.writeObject("Unknown Action");
-                output.flush();
-            } catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
-   
 }
